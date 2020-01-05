cc7c7adaefc3c391615f95099ab593a0481a451a
Joseph Jennings <josephjennings@Josephs-MacBook-Pro.local>
2020-01-05 01:17:59 -0800

    cleaned up code and completely logic

--- src/components/form.js
+++ src/components/form.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-class Form extends React.Component {
-    render(){
-        return (
-            <div className="form">
-                <form onSubmit={this.props.getWeather}>
-                    <input type="text" name="city" placeholder="City..."/>
-                    <input type="text" name="country" placeholder="Country..."/>
-                    <button>Get Weather</button>
-                </form>
-            </div>
-        );
-    }
-}
+const Form = props => (
+    <div className="form">
+        <form onSubmit={props.getWeather}>
+            <input type="text" name="city" placeholder="City..."/>
+            <input type="text" name="country" placeholder="Country..."/>
+            <button>Get Weather</button>
+        </form>
+    </div>
+)
 
 export default Form;
--- src/components/titles.js
+++ src/components/titles.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-function Titles() {
-  return (
-    <div className="titles">
-      <h1>Weather Finder</h1>
-      <p>Find out temperature, conditions, and more...</p>
-    </div>
-  );
-}
+const Titles = () => (
+  <div className="titles">
+    <h1>Weather Finder</h1>
+    <p>Find out temperature, conditions, and more...</p>
+  </div>
+)
 
 export default Titles;
